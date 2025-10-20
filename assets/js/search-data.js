fd2ca795d62c8bae51b5d6432686ca2598612b6b
YiZhong-xybc <123468791+YiZhong-xybc@users.noreply.github.com>
2025-10-20 06:02:01 +0000

    Deploying to gh-pages from @ YiZhong-xybc/YiZhong-xybc.github.io@25c44a940b5a4a255e2f287c36aa3336b086e964 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,31 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "projects-real-time-wheat-stripe-rust-detector",
+        },{id: "projects-real-time-wheat-stripe-rust-detector",
           title: 'Real-time Wheat Stripe-Rust Detector',
-          description: "A lightweight YOLOv5-Lite model running on Raspberry Pi 4B",
+          description: "Undergraduate Research Program",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-tic-tac-toe-human-machine-device",
@@ -67,7 +45,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-cotton-seeding-robot",
           title: 'Cotton Seeding Robot',
-          description: "2023 Int&#39;l Smart Ag-Equipment Contest | Provincial A-Class 2nd Prize",
+          description: "2023 Int&#39;l Smart Ag-Equipment Contest | 2nd Prize",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "researches-trajectory-planning-for-multi-vehicle-cooperative-parking",
@@ -81,48 +59,6 @@ ninja.data = [{
           section: "Researches",handler: () => {
               window.location.href = "/researches/2_research/";
             },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%7A%68%6F%6E%67%79%69@%63%61%75.%65%64%75.%63%6E", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/zhongyi-xybc", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
